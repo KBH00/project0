dba0874d7155e19466e2373449cd140812ef07c8
sFaster <sfaster@gmail.com>
2023-03-24 20:04:38 +0900

    resize test.

--- src/test0.cpp
+++ src/test0.cpp
@@ -3,5 +3,17 @@
 
 int main(int argc, char **argv) {
 	printf("This test0.\n");
+
+	cv::Mat img = cv::imread("../IMG_4113.JPG", 1);
+	cv::Mat imgResize;
+
+	int rate = 10;
+
+	cv::resize(img, imgResize, cv::Size(img.cols/rate, img.rows/rate));
+
+
+	cv::imshow("test", imgResize);
+	cv::waitKey(0);
+
 	return 0;
 }
